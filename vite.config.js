551f2789d9c4f20c651121e1988143b70b222205
عبدالملك العباوي <abdulmalek.23cs56@student.uomosul.edu.iq>
2025-07-31 04:21:18 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true
-  }
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true
+      }
+    })
+  ]
 })
