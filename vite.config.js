2251e82c2460c1fa44bf7c2ad9d276d631564839
عبدالملك العباوي <abdulmalek.23cs56@student.uomosul.edu.iq>
2025-07-31 04:17:06 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr() // لتحسين استيراد ملفات SVG
+  ],
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    assetsInlineLimit: 4096 // حجم الحد للأصول المضمنة (بالبايت)
+  },
+  resolve: {
+    alias: {
+      '@': '/src' // ميزة المسارات المطلقة
+    }
+  },
+  server: {
+    port: 3000, // منفذ التطوير
+    open: true // فتح المتصفح تلقائياً
   }
 })
